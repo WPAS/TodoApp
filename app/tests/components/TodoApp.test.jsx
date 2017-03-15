6f7612860f63ebb696942aeba472d4bd5c6993d7
Wojciech Pastuszka <wojciech.pastuszka@gazeta.pl>
2017-03-15 13:22:32 +0100

    complete handleAddTodo

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -10,4 +10,14 @@ describe("TodoApp", () => {
   it("should exist", () => {
       expect(TodoApp).toExist();
   });
+
+  it("should add todo to the state.todos on handleAddTodo ", () => {
+    var todoText = "test some component";
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp />)
+
+    todoApp.setState({todos: []});
+    todoApp.handleAddTodo(todoText);
+
+    expect(todoApp.state.todos[0].text).toBe(todoText);
+  });
 });
