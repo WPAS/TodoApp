b423b2b20771f5e0a8acfba3cd7942923f7dbd16
Wojciech Pastuszka <wojciech.pastuszka@gazeta.pl>
2017-03-23 10:46:11 +0100

    add time info into todos

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,15 +1,28 @@
 var React = require("react");
+var moment = require("moment");
 
 var Todo = React.createClass({
   render: function() {
-    var { text, id, completed } = this.props;
+    var { text, id, completed, createdAt, completedAt } = this.props;
+    var renderDate = () => {
+      var message = 'Dodano ';
+      var timestamp = createdAt;
+
+      if (completed) {
+        message = "Ukończone ";
+        timestamp = completedAt;
+      }
+
+      return message + moment.unix(timestamp).format('D.M.YYYY r. o kk:mm');
+    };
 
     return (
       <div onClick={() => {
             this.props.onToggle(id);
         }}>
         <input type="checkbox" checked={completed} />
-        {text}
+        <p>{text}</p>
+        <p>{renderDate()}</p>
       </div>
     )
   }
